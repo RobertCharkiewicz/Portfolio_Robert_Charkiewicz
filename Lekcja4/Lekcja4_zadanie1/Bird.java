4110579269b6f109e8caf3754c8b94696b74e9f7
Robert <robert.charkiewicz@gmail.com>
2019-12-13 13:38:55 +0100

    Lekcja 4 zadani 1

---
+++ Lekcja4/Lekcja4_zadanie1/Bird.java
@@ -0,0 +1,18 @@
+package Lekcja4_zadanie1;
+
+public class Bird extends Animal {
+
+    public Bird(String name, int numberOfLegs, int numberOfWings) {
+        super(name, numberOfLegs, numberOfWings);
+    }
+
+    @Override
+    public void fly() {
+        System.out.print("Bird is flying, ");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("not swimming.");
+    }
+}
---
+++ Lekcja4/Lekcja4_zadanie1/Dog.java
@@ -0,0 +1,18 @@
+package Lekcja4_zadanie1;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int numberOfLegs, int numberOfWings) {
+        super(name, numberOfLegs, numberOfWings);
+    }
+
+    @Override
+    public void fly() {
+        System.out.print("Dog is nor flying ");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("either swimming.");
+    }
+}
