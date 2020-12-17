ca319d414ad91d0ac23cb565afd1ef3100be1ca6
robson65 <robert.charkiewicz@gmail.com>
2020-12-17 15:17:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio_Robert_Charkiewicz
--- src/main/java/Lekcja1/MyFirstClass.java
+++
@@ -1,9 +0,0 @@
-package Lekcja1;
-
-public class MyFirstClass {
-
-    public static void main(String[] args)   {
-
-        System.out.print ("Hello World here is Robert");
-    }
-}
